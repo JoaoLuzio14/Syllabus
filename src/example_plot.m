c80bd743e1f49d9c5bd971bc8deafb1d4d958bdc
marcelof-15 <marcelojsforte@gmail.com>
2022-01-23 19:05:37 +0000

    Update example_plot.m

--- src/example_plot.m
+++ src/example_plot.m
@@ -15,8 +15,9 @@ h = image(xlim,-ylim,I);
 uistack(h,'bottom')
 
 % Vehicle Model
-x = [-1.666, -1.666, 1.666, 1.666];
-y = [  0.754,  -0.754, -0.754, 0.754];
+x = [-0.566, -0.566, 2.766, 2.766];
+y = [  0.64,  -0.64, -0.64, 0.64];
+
 g = hgtransform;
 myShape2 = patch('XData',x,'YData',y,'FaceColor','red','Parent',g);
 set(myShape2,'Xdata',x,'Ydata',y);
@@ -26,9 +27,7 @@ set(myShape2,'Xdata',x,'Ydata',y);
 for k=1:20:(p_final-1)
     disp(k);
     pt1 = [out.state(k,1) out.state(k,2) 0];
-    pt2 = [out.state(k+1,1) out.state(k+1,2) 0];
-    r1 = out.state(k,3)*pi/180;
-    r2 = out.state(k+1,3)*pi/180;
+    r1 = out.state(k,3);
     g.Matrix = makehgtform('translate',pt1,'zrotate',r1);
     drawnow
 end
